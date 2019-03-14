120c782e5d05d213f5cedcdede47c2357fe065dd
sorensms <sorensms@rose-hulman.edu>
2019-03-14 11:02:57 -0400

    Done

--- src/m3.py
+++ src/m3.py
@@ -122,20 +122,20 @@ def broken_1(m):
     #    **  For full credit you must appropriately
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     count = 0
-    count2=0
     b=0
     for k in range((2 * m)+2-m):
-        b=b+1
+
         a=is_prime(m+b)
-        count2=count2+1
+
         if a==True:
             count = count + 1
+        b=b+1
     return count
     print(count)
-    print(count2)
+
 
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
-    main()
+main()
